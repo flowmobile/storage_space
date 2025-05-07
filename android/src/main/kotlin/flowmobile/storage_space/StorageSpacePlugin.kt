2bd7e3a62251e3b854a0611a510a4af29a3e25dc
Radee Jamil Khan <radee9999@gmail.com>
2025-05-08 02:31:57 +0600

    kotlin version upgrade

--- android/src/main/kotlin/flowmobile/storage_space/StorageSpacePlugin.kt
+++ android/src/main/kotlin/flowmobile/storage_space/StorageSpacePlugin.kt
@@ -1,64 +1,40 @@
 package flowmobile.storage_space
 
-import androidx.annotation.NonNull;
-
+import android.os.Environment
+import android.os.StatFs
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import  android.os.StatFs
-import  android.os.Environment
 
-/** StorageSpacePlugin */
-public class StorageSpacePlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+/** StorageSpacePlugin - Flutter Plugin (v2 embedding only) */
+class StorageSpacePlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
+  private lateinit var channel: MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "storage_space")
-    channel.setMethodCallHandler(this);
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "storage_space")
+    channel.setMethodCallHandler(this)
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "storage_space")
-      channel.setMethodCallHandler(StorageSpacePlugin())
-    }
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if(call.method == "getFreeSpace"){
-      val stat = StatFs(Environment.getDataDirectory().getPath())
-      val bytesAvailable = stat.getFreeBytes()
-      result.success(bytesAvailable)
-    }
-    else if(call.method == "getTotalSpace"){
-      val stat = StatFs(Environment.getDataDirectory().getPath())
-      val bytesTotal = stat.getTotalBytes()
-      result.success(bytesTotal)
-    }
-    else {
-      result.notImplemented()
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+    when (call.method) {
+      "getFreeSpace" -> {
+        val stat = StatFs(Environment.getDataDirectory().path)
+        val bytesAvailable = stat.freeBytes
+        result.success(bytesAvailable)
+      }
+      "getTotalSpace" -> {
+        val stat = StatFs(Environment.getDataDirectory().path)
+        val totalBytes = stat.totalBytes
+        result.success(totalBytes)
+      }
+      else -> {
+        result.notImplemented()
+      }
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
-
 }
